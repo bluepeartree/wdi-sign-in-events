e05172e9a53ba931ff9297e9d3c997ee3715a5a2
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-21 16:29:20 -0500

    added comments

--- js/main.js
+++ js/main.js
@@ -6,19 +6,21 @@ var modalClose = modal.querySelector('body > div > form > button.close');
 var submitButton = modal.querySelector('.submit');
 var inputs = modal.querySelectorAll('input');
 
+// to display sign in block
 signIn.addEventListener('click', function()
 {
   modal.style.display = 'block';
 }
 )
 
+//to remove sign in block
 modalClose.addEventListener('click', function()
 {
   modal.style.display = 'none';
 }
 
 )
-
+//generate error if click submit without enter fields
 submitButton.addEventListener('click', function(e)
 {
   inputs.forEach(function(input)
@@ -29,6 +31,7 @@ submitButton.addEventListener('click', function(e)
 )
 })
 
+//clicking in field removes error class
 inputs.forEach(function(input)
 {
   input.addEventListener('click', function(e)
@@ -37,13 +40,11 @@ inputs.forEach(function(input)
   )
 })
 
+//clicking anywhere except input shields shuts down sign in display
 modal.addEventListener('click', function()
 {
   modal.style.display = 'none';
-
-}
-
-)
+})
 
 
 
